630495d92fdac4d335d53e7fab95aa422564eff3
Steve Witman <stevewitman@gmail.com>
2021-11-17 23:07:34 -0700

    add firebase and emulators
      - add firebase 9.4.0 and angularfire 7.2.0
      - setup firebase configuration with environment for dev and prod
      - add getPosts method to data service to verify connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  useEmulators: false,
+  firebase: {
+    projectId: 'skiareanewsdotcom',
+    appId: '1:737572959184:web:657958233d60cde0977fd5',
+    storageBucket: 'skiareanewsdotcom.appspot.com',
+    apiKey: 'AIzaSyDqlw2KT0k5k8Wvo6y6aqe0ETuaU6fY1do',
+    authDomain: 'skiareanewsdotcom.firebaseapp.com',
+    messagingSenderId: '737572959184',
+    measurementId: 'G-Q74JBCQLSK',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  useEmulators: true,
+  firebase: {
+    projectId: 'skiareanewsdotcom',
+    appId: '1:737572959184:web:657958233d60cde0977fd5',
+    storageBucket: 'skiareanewsdotcom.appspot.com',
+    apiKey: 'AIzaSyDqlw2KT0k5k8Wvo6y6aqe0ETuaU6fY1do',
+    authDomain: 'skiareanewsdotcom.firebaseapp.com',
+    messagingSenderId: '737572959184',
+    measurementId: 'G-Q74JBCQLSK',
+  },
 };
 
 /*
